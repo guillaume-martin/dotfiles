041b4e3a7387a6b7c15fc04f175b2cddbcf6c369
Guillaume Martin <8098412+guillaume-martin@users.noreply.github.com>
2023-03-01 21:36:01 +0800

    Corrects tmux display settings

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -2,6 +2,7 @@
 -- https://libreddit.kavin.rocks/r/neovim/comments/11a62hj/color_schemes_seem_to_be_highlighting_incorrectly/
 
 
+vim.o.termguicolors = true
 vim.cmd.colorscheme("gruvbox")
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
