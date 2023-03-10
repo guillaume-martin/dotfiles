5224491d2ce41bd651f1636ef7ea7db713ebccec
Guillaume Martin <8098412+guillaume-martin@users.noreply.github.com>
2023-03-10 14:50:47 +0800

    Adds some remap to nvim

--- .config/nvim/lua/custom/remap.lua
+++ .config/nvim/lua/custom/remap.lua
@@ -2,3 +2,16 @@
 vim.g.mapleader = " " 
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Move between panes
+vim.keymap.set("n", "<leader>h", "<C-w>h")
+vim.keymap.set("n", "<leader>l", "<C-w>l")
+vim.keymap.set("n", "<leader>j", "<C-w>j")
+vim.keymap.set("n", "<leader>k", "<C-w>k")
+
+-- Stay centered
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
